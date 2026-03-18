ca661cb8f0d1480fe4b46e9c52e7dc2c2ae5098e
Dmitrii Patsura <talk@dmtry.me>
2026-03-18 23:02:31 +0100

    feat: Attention + SkipConcatConv loop interchange: AA total 14ms→13ms (-6%)

--- v0/aa/small/shaders/5_AttentionCS.hlsl
+++ v0/aa/small/shaders/5_AttentionCS.hlsl
@@ -14,30 +14,35 @@ void main(uint3 dtid : SV_DispatchThreadID)
     uint idx = y * g_Width + x;
     uint C = g_InChannels; // 32
 
-    for (uint oc = 0; oc < C; oc++)
+    // Debug mode 3: skip temporal fusion, pass current features only
+    if (g_DebugMode == 3) {
+        for (uint oc = 0; oc < C; oc++)
+            g_Features[outBase + oc * hw + idx] = g_Features[currBase + oc * hw + idx];
+        return;
+    }
+
+    #define MAX_ATT_CH 32
+    float sums[MAX_ATT_CH];
+    for (uint oc = 0; oc < C; oc++) sums[oc] = 0.0;
+
+    for (uint ic = 0; ic < C * 2; ic++)
     {
-        float curr_val = g_Features[currBase + oc * hw + idx];
+        float val;
+        if (ic < C)
+            val = g_Features[currBase + ic * hw + idx];
+        else
+            val = g_Features[warpBase + (ic - C) * hw + idx];
+        for (uint oc = 0; oc < C; oc++)
+            sums[oc] += val * g_Weights[g_WeightOff + oc * (C * 2) + ic];
+    }
 
-        // Debug mode 3: skip temporal fusion, pass current features only
-        if (g_DebugMode == 3) {
-            g_Features[outBase + oc * hw + idx] = curr_val;
-            continue;
-        }
-
-        float sum = 0.0;
-        for (uint ic = 0; ic < C * 2; ic++)
-        {
-            float val;
-            if (ic < C)
-                val = g_Features[currBase + ic * hw + idx];
-            else
-                val = g_Features[warpBase + (ic - C) * hw + idx];
-            sum += val * g_Weights[g_WeightOff + oc * (C * 2) + ic];
-        }
-        sum += g_Weights[g_BiasOff + oc];
+    for (uint oc2 = 0; oc2 < C; oc2++)
+    {
+        float sum = sums[oc2] + g_Weights[g_BiasOff + oc2];
         float attn = 1.0 / (1.0 + exp(-sum));
 
-        float warp_val = g_Features[warpBase + oc * hw + idx];
-        g_Features[outBase + oc * hw + idx] = curr_val + warp_val * attn;
+        float curr_val = g_Features[currBase + oc2 * hw + idx];
+        float warp_val = g_Features[warpBase + oc2 * hw + idx];
+        g_Features[outBase + oc2 * hw + idx] = curr_val + warp_val * attn;
     }
 }
--- v0/aa/small/shaders/7_SkipConcatConvCS.hlsl
+++ v0/aa/small/shaders/7_SkipConcatConvCS.hlsl
@@ -15,21 +15,26 @@ void main(uint3 dtid : SV_DispatchThreadID)
     uint totalIn = g_InChannels;
     uint halfIn = totalIn / 2;
 
-    for (uint oc = 0; oc < g_OutChannels; oc++)
-    {
-        float sum = 0.0;
-        for (uint ic = 0; ic < totalIn; ic++)
-        {
-            float val;
-            if (ic < halfIn)
-                val = g_Features[inBase + ic * hw + idx];
-            else
-                val = g_Features[auxBase + (ic - halfIn) * hw + idx];
-            sum += val * g_Weights[g_WeightOff + oc * totalIn + ic];
-        }
-        if (g_BiasOff != 0xFFFFFFFF)
-            sum += g_Weights[g_BiasOff + oc];
+    #define MAX_OUT_CH 32
+    float sums[MAX_OUT_CH];
+    for (uint oc = 0; oc < g_OutChannels; oc++) sums[oc] = 0.0;
 
-        g_Features[outBase + oc * hw + idx] = sum;
+    for (uint ic = 0; ic < totalIn; ic++)
+    {
+        float val;
+        if (ic < halfIn)
+            val = g_Features[inBase + ic * hw + idx];
+        else
+            val = g_Features[auxBase + (ic - halfIn) * hw + idx];
+        for (uint oc = 0; oc < g_OutChannels; oc++)
+            sums[oc] += val * g_Weights[g_WeightOff + oc * totalIn + ic];
+    }
+
+    for (uint oc2 = 0; oc2 < g_OutChannels; oc2++)
+    {
+        float s = sums[oc2];
+        if (g_BiasOff != 0xFFFFFFFF)
+            s += g_Weights[g_BiasOff + oc2];
+        g_Features[outBase + oc2 * hw + idx] = s;
     }
 }
